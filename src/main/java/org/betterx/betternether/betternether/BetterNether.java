b3091cab3f398566c96173f17a8b9bb27fd7ebb5
ExoticCandyC <kimia.azizollahi@gmail.com>
2025-02-17 16:54:03 +0100

    initial commit

--- src/main/java/org/betterx/betternether/betternether/BetterNether.java
+++ src/main/java/org/betterx/betternether/betternether/BetterNether.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 
 
-
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
